05948b7f5b1089fabd9f6bba6df83ebe9677c54a
Pedro Escalante <pedro.escalante@citriom.com>
2016-02-24 09:42:51 -0430

    Adding Routes

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Tekahto - Laravel project
+## Tekahto - Laravel application using InfusionSoft SKD
+=======================================================
+
+###### Description
+
+This is a test application using Laravel PHP Framework to retrieve and send data to the InfusionSoft API, using their PHP SDK. 
+
+
+###### License
+
+The Laravel framework is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT)
--- app/routes.php
+++ app/routes.php
@@ -19,44 +19,44 @@ Route::get('/', function()
 Route::get('/infusionsoft/callback', [ function()
 {
     $infusionsoft = new Infusionsoft\Infusionsoft(array(
-            'clientId'     => $_ENV['clientId'],
-            'clientSecret' => $_ENV['clientSecret'],
-            'redirectUri'  => $_ENV['redirectUri']
-        ));
+        'clientId'     => $_ENV['clientId'],
+        'clientSecret' => $_ENV['clientSecret'],
+        'redirectUri'  => $_ENV['redirectUri']
+    ));
 
-        // If the serialized token is available in the session storage, we tell the SDK
-        // to use that token for subsequent requests.
-        if (isset($_SESSION['token'])) {
-            $infusionsoft->setToken(unserialize($_SESSION['token']));
-        }
+    // If the serialized token is available in the session storage, we tell the SDK
+    // to use that token for subsequent requests.
+    if (isset($_SESSION['token'])) {
+        $infusionsoft->setToken(unserialize($_SESSION['token']));
+    }
 
-        // If we are returning from Infusionsoft we need to exchange the code for an
-        // access token.
-        if (isset($_GET['code']) and !$infusionsoft->getToken()) {
-            //$infusionsoft->requestAccessToken($_GET['code']);
-    	    $client = new \GuzzleHttp\Client();
-    	    $data = [
-    		'client_id' => $_ENV['client_id'],
-    		'client_secret' => $_ENV['clientSecret'],
-    		'code' => $_GET['code'],
-    		'grant_type' => 'authorization_code',
-    		'redirectUri'  => $_ENV['redirectUri']
+    // If we are returning from Infusionsoft we need to exchange the code for an
+    // access token.
+    if (isset($_GET['code']) and !$infusionsoft->getToken()) {
+        //$infusionsoft->requestAccessToken($_GET['code']);
+	    $client = new \GuzzleHttp\Client();
+	    $data = [
+		'client_id' => $_ENV['clientId'],
+		'client_secret' => $_ENV['clientSecret'],
+		'code' => $_GET['code'],
+		'grant_type' => 'authorization_code',
+		'redirectUri'  => $_ENV['redirectUri']
 
-    	    ];
-    	    var_dump($data);
-    	    $response = $client->post('https://api.infusionsoft.com/token', $data);
-    	    var_dump($response);
-        }
+	    ];
+	    var_dump($data);
+	    $response = $client->post('https://api.infusionsoft.com/token', $data);
+	    var_dump($response);
+    }
 
-        if( isset($_GET) )
-            var_dump($_GET);
+    if( isset($_GET) )
+        var_dump($_GET);
 
-        if ($infusionsoft->getToken()) {
-            // Save the serialized token to the current session for subsequent requests
-            $_SESSION['token'] = serialize($infusionsoft->getToken());
+    if ($infusionsoft->getToken()) {
+        // Save the serialized token to the current session for subsequent requests
+        $_SESSION['token'] = serialize($infusionsoft->getToken());
 
-            // MAKE INFUSIONSOFT REQUEST
-        } else {
-            echo '<a href="' . $infusionsoft->getAuthorizationUrl() . '">Click here to authorize</a>';
-        }
+        // MAKE INFUSIONSOFT REQUEST
+    } else {
+        echo '<a href="' . $infusionsoft->getAuthorizationUrl() . '">Click here to authorize</a>';
+    }
 }]);
