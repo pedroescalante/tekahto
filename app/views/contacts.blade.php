d3bd766e545ee85952f4c0d4d23fb398d28fe714
Pedro Escalante <pedro.escalante@citriom.com>
2016-02-25 08:51:33 -0430

    Blade Templating

--- app/views/contacts.blade.php
+++ app/views/contacts.blade.php
@@ -1,51 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
+@extends('layouts.master')
 
-    <title>InfusionSoft Contact List</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="/css/jumbotron-narrow.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
-  <body>
-
-    <div class="container">
-      <div class="header clearfix">
-        <nav>
-          <ul class="nav nav-pills pull-right">
-            <li role="presentation" class="active"><a href="#">Home</a></li>
-            <li role="presentation"><a href="#">About</a></li>
-            <li role="presentation"><a href="#">Contact</a></li>
-          </ul>
-        </nav>
-        <h3 class="text-muted">InfusionSoft API</h3>
-      </div>
-
-      <div class="row marketing">
+@section('content')
         <div class="col-sm-12">
           <h4>Contacts</h4>
           <table class="table table-striped">
@@ -57,24 +12,12 @@
             </tr>
             @foreach($contacts as $contact)
             <tr>
-              <td> {$contact['ID']}         </td>
-              <td> {$contact['FirstName']}  </td>
-              <td> {$contact['LastName']}   </td>
-              <td> {$contact['Email']}      </td>
+              <td> {{$contact['ID']}}         </td>
+              <td> {{$contact['FirstName']}}  </td>
+              <td> {{$contact['LastName']}}   </td>
+              <td> {{$contact['Email']}}      </td>
             </tr>
             @endforeach
           </table>
         </div>
-      </div>
-
-      <footer class="footer">
-        <p>&copy; 2016 Citriom</p>
-      </footer>
-
-    </div> <!-- /container -->
-
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
-</html>
+@stop      
