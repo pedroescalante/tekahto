dbbfadb097405205eb5f31ad662d521d2f29b217
Pedro Escalante <pedro.escalante@citriom.com>
2016-02-25 11:25:17 -0430

    no message

--- app/views/product.blade.php
+++ app/views/product.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.master')
-<?php var_dump($product); ?>
 @section('content')
         <div class="col-sm-12">
           <h4>Product</h4>
           <table class="table table-striped">
             <tr>
               <th> ID         </th>
-              <td> {{ $product['Id'] }} </td>
+              <td> {{ $product['id'] }} </td>
             </tr>
             <tr>
               <th> Name         </th>
@@ -14,11 +13,15 @@
             </tr>
             <tr>
               <th> Price         </th>
-              <td> {{ $product['ProductPrice'] }} </td>
+              <td> {{ $product['product_price'] }} </td>
             </tr>
             <tr>
               <th> Status </th>
               <td> {{ $product['Status'] }} </td>
+            </tr>
+            <tr>
+              <th> Description </th>
+              <td> {{ $product['product_short_desc'] }} </td>
             </tr>
           </table>
         </div>
