5f67708307c09a47c9b73b385d36a4d002c89d27
Pedro Escalante <pedro.escalante@citriom.com>
2016-02-25 11:02:20 -0430

    no message

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -37,9 +37,8 @@
       <div class="header clearfix">
         <nav>
           <ul class="nav nav-pills pull-right">
-            <li role="presentation" class="active"><a href="#">Home</a></li>
-            <li role="presentation"><a href="#">About</a></li>
-            <li role="presentation"><a href="#">Contact</a></li>
+            <li role="presentation"><a href="/contacts">Contacts</a></li>
+            <li role="presentation"><a href="/products">Products</a></li>
           </ul>
         </nav>
         <h3 class="text-muted">InfusionSoft API</h3>
