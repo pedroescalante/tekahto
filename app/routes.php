6ea6d0728db9ae6018faf59392f8ac32a43d15dd
Pedro Escalante <pedro.escalante@citriom.com>
2016-03-10 16:59:25 -0430

    no message

--- app/routes.php
+++ app/routes.php
@@ -5,8 +5,12 @@ Route::get('/', function(){ return View::make('hello'); });
 Route::get('/infusionsoft',             ['uses' =>'InfusionsoftController@getLink']);
 Route::get('/infusionsoft/callback',    ['https', 'uses' => 'InfusionsoftController@callback']);
 Route::get('/infusionsoft/token',       ['uses' => 'InfusionsoftController@sendToken']);
-Route::get('/infusionsoft/contacts',    ['uses' => 'InfusionsoftController@contacts']);
+//Route::get('/infusionsoft/contacts',    ['uses' => 'InfusionsoftController@contacts']);
 Route::get('/infusionsoft/contact',     ['uses' => 'InfusionsoftController@contact']);
 Route::get('/infusionsoft/products',    ['uses' => 'InfusionsoftController@products']);
 Route::get('/infusionsoft/product',     ['uses' => 'InfusionsoftController@product']);
 Route::get('/infusionsoft/invoice',     ['uses' => 'InfusionsoftController@invoice']);
+
+Route::get('/infusionsoft/contacts', function(){
+    echo "Contacts";
+});
