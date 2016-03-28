b71a498a8dbd2d244982686b91175089c3012fce
pedroescalante <pedro.escalante@citriom.com>
2016-03-28 16:49:29 -0400

    Add POST route

--- app/routes.php
+++ app/routes.php
@@ -5,6 +5,7 @@ Route::get('/', function(){ return View::make('hello'); });
 Route::get('/infusionsoft',             ['https', 'uses' =>'InfusionsoftController@getLink']);
 Route::get('/infusionsoft/callback',    ['https', 'uses' => 'InfusionsoftController@callback']);
 Route::get('/infusionsoft/token',       ['https', 'uses' => 'InfusionsoftController@sendToken']);
+Route::post('/infusionsoft/token', 	['https', 'uses' => 'InfusionsoftController@sendToken']);
 Route::get('/infusionsoft/contacts',    ['https', 'uses' => 'InfusionsoftController@contacts']);
 Route::get('/infusionsoft/contact',     ['https', 'uses' => 'InfusionsoftController@contact']);
 Route::get('/infusionsoft/products',    ['https', 'uses' => 'InfusionsoftController@products']);
