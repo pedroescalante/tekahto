9c2730d6fd3e817c1db124facd1fc576d2b0b0c5
Pedro Escalante <pedro.escalante@citriom.com>
2016-03-29 09:36:01 -0430

    no message

--- app/controllers/InfusionsoftController.php
+++ app/controllers/InfusionsoftController.php
@@ -291,25 +291,32 @@ class InfusionsoftController extends BaseController {
 	    	$token->token = serialize($infusionsoft->getToken());
 	    	$token->save();
 
-	        $product = $infusionsoft->data->query(
+	        $data['product'] = $infusionsoft->data->query(
 	                    'Product',
 	                    10, 0,
 	                    ['ID' => $plan_id],
 	                    ['Id', 'ProductName', 'Description', 'ProductPrice', 'Status'],
 	                    'ProductName',
 	                    true);
+
 	        $contacts = $infusionsoft->contacts->findByEmail($email, ['Id', 'FirstName', 'LastName']);
-	        foreach($contacts as $contact){
+			foreach ($contacts as $contact) 
+		    {
+		        $c = $infusionsoft->contacts->load($contact['Id'], ['Id', 'FirstName', 'LastName']);
+
 		        $credit_cards = $infusionsoft->data->query(
 		                    'CreditCard',
 		                    10, 0,
-		                    ['ContactID' => $contact['Id']],
-		                    ['CardType', 'Last4'],
+		                    ['ContactID' => $c['Id']],
+		                    ['CardType', 'Last4', 'Status'],
 		                    'Last4',
 		                    true);
-		     }
+		        $c['CreditCards'] = $credit_cards;
+
+		        $data['contact'] = $c;
+		    }
 	    }
 
-	    return Response::json([$product, $contacts[0], $credit_cards]);
+	    return Response::json($data);
 	}
 }
