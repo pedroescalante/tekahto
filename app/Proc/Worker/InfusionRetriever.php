5a2f7841d474db542d7ca94e5dcfce9a7cc93d30
Pedro Escalante <pedro.escalante@citriom.com>
2017-01-30 21:21:57 -0400

    Class notation

--- app/Proc/Worker/InfusionRetriever.php
+++ app/Proc/Worker/InfusionRetriever.php
@@ -8,7 +8,7 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
 use Log;
 use Infusionsoft\Infusionsoft;
-use app\models\Token;
+use Token;
 
 class InfusionRetriever 
 {
@@ -57,52 +57,57 @@ class InfusionRetriever
 
 	public function fire($job, $package)
 	{
-		$infusionsoft = $this->getInfusionsoftObject();
-		$infusionsoft = $this->refreshTokenTwo($infusionsoft);
+		try 
+		{
+			$infusionsoft = $this->getInfusionsoftObject();
+			$infusionsoft = $this->refreshTokenTwo($infusionsoft);
 
-		$contacts = $infusionsoft->contacts->findByEmail($data['email'], ['Id', 'FirstName', 'LastName', 'Phone1']);
+			$contacts = $infusionsoft->contacts->findByEmail($data['email'], ['Id', 'FirstName', 'LastName', 'Phone1']);
 
-		if( !isset($contacts[0]) ) {
-			$package['plan_count'] = 0;
-			$package['plans']	   = [];
-		}
-		else {
-			$contact = $infusionsoft->contacts->load($contacts[0]['Id'], ['Id', 'FirstName', 'LastName', 'Phone1']);
-			$subs = $this->getSubscriptionsAllData($infusionsoft, $contact['Id']);
-			$subs_array =[];
-			foreach($subs as $sub)
-			{
-				/*if( isset($products[$sub['ProductId']]['ProductName']) )
-					$sub['ProductName'] = $products[$sub['ProductId']]['ProductName'];
-				else
-					$sub['ProductName'] = "";
-				
-				if( isset($merchants[$sub['merchantAccountId']]) )
-					$sub['Merchant'] = $merchants[ $sub['merchantAccountId'] ];
-				else
-					$sub['Merchant'] = "Merchant: ".$sub['merchantAccountId'];
-
-				if( isset($billcycle[$sub['BillingCycle']]) )
-					$sub['BillingCycle'] = $billcycle[ $sub['BillingCycle'] ];
-
-				if( $sub['AutoCharge'] == 1 ) $sub['AutoCharge'] = "Yes"; else $sub['AutoCharge'] = "No";
-				*/
-				if( isset( $sub['StartDate']) )
-					$sub['StartDate'] 	 = $sub['StartDate']->format('Y-m-d H:i:s');
-				if( isset( $sub['LastBillDate']) )
-					$sub['LastBillDate'] = $sub['LastBillDate']->format('Y-m-d H:i:s');
-				if( isset( $sub['NextBillDate']) )
-					$sub['NextBillDate'] = $sub['NextBillDate']->format('Y-m-d H:i:s');
-
-				$subs_array[] = $sub;
+			if( !isset($contacts[0]) ) {
+				$package['plan_count'] = 0;
+				$package['plans']	   = [];
 			}
-			$contact['subscriptions'] = $subs_array;
+			else {
+				$contact = $infusionsoft->contacts->load($contacts[0]['Id'], ['Id', 'FirstName', 'LastName', 'Phone1']);
+				$subs = $this->getSubscriptionsAllData($infusionsoft, $contact['Id']);
+				$subs_array =[];
+				foreach($subs as $sub)
+				{
+					/*if( isset($products[$sub['ProductId']]['ProductName']) )
+						$sub['ProductName'] = $products[$sub['ProductId']]['ProductName'];
+					else
+						$sub['ProductName'] = "";
+					
+					if( isset($merchants[$sub['merchantAccountId']]) )
+						$sub['Merchant'] = $merchants[ $sub['merchantAccountId'] ];
+					else
+						$sub['Merchant'] = "Merchant: ".$sub['merchantAccountId'];
 
-			$package['plan_count'] = count( $contact['subscriptions'] );
-			$package['plans']      = $contact['subscriptions'];
+					if( isset($billcycle[$sub['BillingCycle']]) )
+						$sub['BillingCycle'] = $billcycle[ $sub['BillingCycle'] ];
+
+					if( $sub['AutoCharge'] == 1 ) $sub['AutoCharge'] = "Yes"; else $sub['AutoCharge'] = "No";
+					*/
+					if( isset( $sub['StartDate']) )
+						$sub['StartDate'] 	 = $sub['StartDate']->format('Y-m-d H:i:s');
+					if( isset( $sub['LastBillDate']) )
+						$sub['LastBillDate'] = $sub['LastBillDate']->format('Y-m-d H:i:s');
+					if( isset( $sub['NextBillDate']) )
+						$sub['NextBillDate'] = $sub['NextBillDate']->format('Y-m-d H:i:s');
+
+					$subs_array[] = $sub;
+				}
+				$contact['subscriptions'] = $subs_array;
+
+				$package['plan_count'] = count( $contact['subscriptions'] );
+				$package['plans']      = $contact['subscriptions'];
+			}
+
+			Log::info("Email: ".$package['email']." - Plans: ".$package['plan_count'] );
+		} catch (Exception $e) {
+			Log::error("Error: ".$e->getMessage());
 		}
-
-		Log::info("Email: ".$package['email']." - Plans: ".$package['plan_count'] );
 
 		$job->delete();
 	}
